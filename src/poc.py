f8905d4216d9881225b59bdc0d66bd13d5dbd9c2
Ariel Alon <arielalon.12321@gmail.com>
2025-11-29 00:43:04 +0200

    src: poc.py: Acheive arbitrary syscall execution

--- src/poc.py
+++ src/poc.py
@@ -8,23 +8,62 @@
 
 from __future__ import annotations
 
+import ctypes
 import io
 import os
+import struct
 import time
 from dataclasses import dataclass
+from enum import Enum
 from pathlib import Path
-from typing import List, Optional
+from typing import ClassVar, List, Optional, Tuple
 
 from elftools.elf.elffile import ELFFile
 from rich.console import Console
+from rich.markup import escape
 
 CLOCK_GETTIME_SYMBOL: str = "__kernel_clock_gettime"
 CLOCK_GETTIME_SHELLCODE = bytes(
     [0x28, 0x0E, 0x80, 0xD2, 0x01, 0x00, 0x00, 0xD4, 0xC0, 0x03, 0x5F, 0xD6]
 )
+GETPID_SYSCALL_NUMBER = 172
 
-
+SYSCALL_CONFIG_OFFSET = 8
+ANY_SYSCALL_SHELLCODE = bytes.fromhex(
+    "140000141f2003d5000000000000000071000000000000000000000000000000"
+    "0000000000000000000000000000000000000000000000000000000000000000"
+    "00000000000000000000000000000000280e80d2010000d4f30300aa69fdff10"
+    "2a0140b9ea01003449fdff10280140f949fdff10200140f9210540f9220940f9"
+    "230d40f9241140f9251540f9010000d4c9fdff102a0140f94a0000b4400100f9"
+    "e00313aac0035fd6"
+)
 console = Console()
+
+
+class SyscallConfigMode(Enum):
+    ORIGINAL = 0
+    ARBITRARY = 1
+
+
+@dataclass
+class SyscallConfig:
+    FORMAT: ClassVar[str] = "<IIQ6QQ"
+    SIZE: ClassVar[int] = struct.calcsize(FORMAT)
+
+    mode: SyscallConfigMode
+    syscall_number: int
+    result_address: int
+    args: Tuple[int, int, int, int, int, int] = (0, 0, 0, 0, 0, 0)
+
+    def pack(self) -> bytes:
+        return struct.pack(
+            self.FORMAT,
+            self.mode.value,
+            0,  # padding
+            self.syscall_number,
+            *self.args,
+            self.result_address,
+        )
 
 
 def log(tag: str, message: str) -> None:
@@ -114,6 +153,17 @@ def parse_proc_maps(pid: int) -> List[ProcMap]:
     return [ProcMap.from_str(pid=pid, line=line) for line in lines]
 
 
+def dump_proc_map(proc_map: ProcMap, name: str) -> None:
+    proc_map_data = proc_map.read_bytes()
+    proc_map_dump = Path.home() / "out" / name
+    proc_map_dump = proc_map_dump.with_suffix(".dump")
+    proc_map_dump.parent.mkdir(exist_ok=True)
+    proc_map_dump.write_bytes(proc_map_data)
+    info(
+        f"Dumped [yellow]{escape(proc_map.pathname or "unknown")}[/yellow] process map to {proc_map_dump}"
+    )
+
+
 def main() -> None:
     pid = os.getpid()
     info(f"PID: {pid}")
@@ -140,19 +190,38 @@ def main() -> None:
     write_process_memory(
         pid=pid,
         address=vdso.start + symbol_address,
-        data=CLOCK_GETTIME_SHELLCODE,
+        data=ANY_SYSCALL_SHELLCODE,
     )
-    info(f"Wrote `clock_gettime` shellcode to '{CLOCK_GETTIME_SYMBOL}'")
-
-    vdso_data = vdso.read_bytes()
-    vdso_dump = Path.home() / "out/vdso-dump.bin"
-    vdso_dump.parent.mkdir(exist_ok=True)
-    vdso_dump.write_bytes(vdso_data)
-    info(f"Dumped [yellow]vdso[/yellow] to {vdso_dump}")
+    info(f"Wrote ANY_SYSCALL_SHELLCODE shellcode to '{CLOCK_GETTIME_SYMBOL}'")
+    dump_proc_map(vdso, name="after-shellcode")
 
     # 4. Trigger a call
     info(f"Triggering '{CLOCK_GETTIME_SYMBOL}'")
     info(f"Current time: {time.monotonic()}")
+
+    # 5. Triggering getpid
+
+    syscall_result = ctypes.c_uint64()
+    syscall_config = SyscallConfig(
+        mode=SyscallConfigMode.ARBITRARY,
+        syscall_number=GETPID_SYSCALL_NUMBER,
+        result_address=ctypes.addressof(syscall_result),
+    )
+    info(f"Created a new syscall config: {syscall_config}")
+
+    write_process_memory(
+        pid=pid,
+        address=vdso.start + symbol_address + SYSCALL_CONFIG_OFFSET,
+        data=syscall_config.pack(),
+    )
+    info(f"Patched \\[vdso] inilined data with new config")
+    dump_proc_map(vdso, name="vdso-after-config")
+
+    info(f"Triggering '{CLOCK_GETTIME_SYMBOL}' + `getpid` syscall")
+    info(f"Current time: {time.monotonic()}, syscall_result: {syscall_result.value}")
+    dump_proc_map(vdso, name="vdso-trigger")
+
+    assert pid == syscall_result.value
 
 
 if __name__ == "__main__":
